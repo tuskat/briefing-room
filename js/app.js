35d3f10e15a1e9df93b4aef65b9faa52451e0265
Cedric Marcellin <cedric.marcellin@gmail.com>
2016-01-25 17:16:40 +0800

    speech pace corrected

--- js/app.js
+++ js/app.js
@@ -1,95 +1,129 @@
+/*var $tlt = $('#txt1').textillate({ 
+    autoStart: false,
+    loop: true
+});*/
+var $line1;
+var $line2;
+var next1;
+var next2;
 
+var index = 0;
+
+$('.next').on('click', function () {
+  //temporary
+  if (index <= 0)
+  { 
+   $line1.textillate("out");
+   $line2.textillate('out');
+   $line1.find('.texts li:first').text(next1);
+   $line2.find('.texts li:first').text(next2); 
+   $line1.textillate("in");
+  
+   index++;
+  }
+  
+});
 window.speakToMe = function () {
 
     $(document).ready(function () {
+
         bitsOfDialogue();
 
     });
 }
 
-function textillateAll() {
-    $('#l1').textillate({
-        initialDelay: 500,
+window.resetSpeech = function () {
 
-        in: {
-            delay: 20,
-            effect: 'fadeIn',
-            sequence: true
-        },
-        out: {
-            effect: 'fadeOut',
-            sync: true
-        }
-    });
-    $('#l2').textillate({
-        initialDelay: 2000,
-        in: {
-            delay: 20,
-            effect: 'fadeIn',
-            sequence: true
 
-        },
-        out: {
-            effect: 'fadeOut',
-            sync: true
-        }
-    });
-    $('#l3').textillate({
-        initialDelay: 4000,
-        in: {
-            delay: 20,
-            effect: 'fadeIn',
-            sequence: true
+    document.getElementById("l1").innerHTML = "";
+    document.getElementById("l2").innerHTML = "";
+    bitsOfDialogue();
+    $tlt.textillate('in');
 
-        },
-        out: {
-            effect: 'fadeOut',
-            sync: true
-        }
-    });
-    $('#l4').textillate({
-        initialDelay: 6000,
-        in: {
-            delay: 20,
-            effect: 'fadeIn',
-            sequence: true
-        },
-        out: {
-            effect: 'fadeOut',
-            sync: true
-        }
-    });
 
 }
-
 
 function bitsOfDialogue() {
 
 
 
     $.get("speech.php", function (r) {
-       var result = JSON.parse(r);
-        //console.dir( result[0][0]["content"] );
-    
-       
+        var result = JSON.parse(r);
+        //console.dir( result[0][0]["content"] );     
+     
         document.getElementById("l1").innerHTML += result[0][0]["content"];
         document.getElementById("l2").innerHTML += result[1][0]["content"];
-        document.getElementById("l3").innerHTML += result[2][0]["content"];
-        document.getElementById("l4").innerHTML += result[3][0]["content"];
-
-
-        var x = Math.floor((Math.random() * 3));
+      
+        /*
+          document.getElementById("l3").innerHTML += result[2][0]["content"];
+          document.getElementById("l4").innerHTML += result[3][0]["content"];
+        */
+        next1 = result[2][0]["content"];
+        next2 = result[3][0]["content"];
+        //  var x = Math.floor((Math.random() * 2));
+        var x = 1;
         document.getElementById('diaporama').style.backgroundImage = "url(\"css/images/diapositives/" + x + ".jpg\")";
         document.getElementById('diaporama').style.backgroundSize = "cover";
+
+
         textillateAll();
 
 
+
     });
- 
-    // document.getElementById("dialogText").innerHTML += snippet[i][0]; 
+
+
 }
+function addNextBits() {
+    /* 
+     var nextLineUp = document.createElement("li");
+     nextLineUp.setAttribute("id", lIndex);
+     nextLineUp.innerHTML = movieData.results[0].title;
+     */
+
+    document.getElementById("txt1");
+    document.getElementById("txt2");
+} 
     
-    
+//http://jsfiddle.net/jschr/mGgd5/  
+
+function textillateAll() {
+    $line1 = $('#l1').textillate({
+        loop: false,
+        minDisplayTime: 6000,
+        in: {
+            delay: 50,
+            effect: 'fadeIn',
+            sequence: true,
+            callback: function () {
+                $line2.textillate('start');
+            }
+        },
+       out: {
+            effect: 'fadeOut',
+            sync: true,
+          
+        }
+
+    });
+
+    $line2 = $('#l2').textillate({
+
+        loop: false,
+        autoStart: false,
+        minDisplayTime: 6000,
+        in: {
+            delay: 50,
+            effect: 'fadeIn',
+            sequence: true
+
+        },
+        out: {
+            effect: 'fadeOut',
+            sync: true
+        }
+    });
+}
     
    
     
